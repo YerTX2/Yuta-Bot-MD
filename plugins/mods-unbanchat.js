75e527f6c677d3aca5aee7c20c72e6e92f56b3f5
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-07 08:05:44 -0500

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '🍟 *¡Kakaroto-Bot NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '🍟 *¡Yuta-Bot-MD NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '🚩 *¡Kakaroto-Bot YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
+await conn.reply(m.chat, '🚩 *¡Yuta-Bot MD YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
