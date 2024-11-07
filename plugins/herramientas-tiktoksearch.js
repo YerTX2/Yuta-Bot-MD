034bb6ed34da885030f3d53c8625b199ba2c943e
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-07 08:09:12 -0500

    Actualizar herramientas-tiktoksearch.js

--- plugins/herramientas-tiktoksearch.js
+++ plugins/herramientas-tiktoksearch.js
@@ -36,7 +36,7 @@ let handler = async (message, { conn, text, usedPrefix, command }) => {
           mediaType: 1, 
           showAdAttribution: true,
           title: '♡  ͜ ۬︵࣪᷼⏜݊᷼𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨⏜࣪᷼︵۬ ͜ ',
-          body: '<(✿◠‿◠)> kakaroto🔥',
+          body: 'Yura bot MD ',
           previewType: 0, 
           thumbnail: logo5,
           sourceUrl: cn 
