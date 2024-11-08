a7f83a994d5ae0ba6cdf3dbbb8e4e1c193f8d0a2
Diomar-s <diomarlugo405@gmail.com>
2024-11-08 00:36:36 -0400

    Update tools-whatmusic.js

--- plugins/tools-whatmusic.js
+++ plugins/tools-whatmusic.js
@@ -36,6 +36,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['whatmusic *<audio/video>*']
 handler.tags = ['tools']
 handler.command = ['whatmusic', 'shazam']
+handler.group = true
 //handler.limit = 1
 //handler.register = true 
 export default handler
