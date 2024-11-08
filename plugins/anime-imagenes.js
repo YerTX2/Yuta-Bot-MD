3a460fab91003a137c820f8468912b322823db69
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-08 11:45:58 -0500

    Actualizar anime-imagenes.js

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+//import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
 const res = (await axios.get(`https://raw.githubusercontent.com/DevDiegoxyz/YaemoriBot-MD/master/src/JSON/anime-${command}.json`)).data;
 const haha = await res[Math.floor(res.length * Math.random())];
@@ -7,7 +7,7 @@ const messages = [['Imagen 1', dev, await res[Math.floor(res.length * Math.rando
 await conn.sendCarousel(m.chat, '🚩 Resultado de ' + command, '🔎 Anime - ' + command, null, messages, m);
 };
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
-handler.tags = ['anime'];
-handler.group = true;
+//handler.tags = ['anime'];
+//handler.group = true;
 //handler.register = true
-export default handler;
+//export default handler;
