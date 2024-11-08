2d2290d65799a3dcfeabbb72d82213c38fb65c7c
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-08 02:34:30 -0500

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,35 +1,32 @@
 let handler = async (m, { conn, isRowner}) => {
-let _muptime
-let totalreg = Object.keys(global.db.data.users).length
-let totalchats = Object.keys(global.db.data.chats).length
-let pp = 'https://files.catbox.moe/titvug.mp4'
-if (process.send) {
-process.send('uptime')
-_muptime = await new Promise(resolve => {
-process.once('message', resolve)
-setTimeout(resolve, 1000)
-}) * 1000
+        let _muptime
+        let totalreg = Object.keys(global.db.data.users).length
+        let totalchats = Object.keys(global.db.data.chats).length
+        let pp = 'https://tinyurl.com/23q544z8'
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
+  const used = process.memoryUsage()
+  let txt = `
+╭━─━─≪ *Info Goku* ≫─━─━━╮
+ \n`
+      txt += `│  ≡◦ *🌟 Creador ∙* ZxYerSixZx\n`
+      txt += `│  ≡◦ *📚 Grupos Unidos ∙* ${groupsIn.length}\n`
+      txt += `│  ≡◦ *👤 Chats Privados ∙* ${chats.length - groupsIn.length}\n`
+      txt += `│  ≡◦ *💬 Total De Chats ∙* ${chats.length}\n`
+      txt += `│  ≡◦ *🌀 Usuarios Registrados ∙* ${totalreg}\n`
+      txt += `│  ≡◦ *👥 Grupos Registrados ∙* ${totalchats}\n`
+      txt += `│  ≡◦ *🕜 Uptime ∙* ${muptime}\n`
+      txt += `╰━─━━─≪✠≫─━───━╯ `
+await conn.sendFile(m.chat, pp, 'thumbnail.jpg', txt, m)
 }
-let muptime = clockString(_muptime)
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
-const used = process.memoryUsage()
-let goku = `╭─⬣「 YUTA-BOT MD」⬣\n`
-goku += `│ 👑 *Creador ∙* YERTX2 \n`
-goku += `│ 📚 *Grupos Unidos ∙* ${groupsIn.length}\n`
-goku += `│ 👤 *Chats Privados ∙* ${chats.length - groupsIn.length}\n`
-goku += `│ 💬 *Total De Chats ∙* ${chats.length}\n`
-goku += `│ 🇦🇱 *Registrados ∙* ${totalreg}\n`
-goku += `│ ☁️ *Grupos Registrados ∙* ${totalchats}\n`
-goku += `│ 🕜 *Actividad ∙* ${muptime}\n`
-goku += `╰─⬣`
-
-
-handler.help = ['status']
-handler.tags = ['info']
-handler.command = /^(estado|status|estate|state|stado|stats)$/i
-//handler.register = true
-export default handler
 
 function clockString(ms) {
 let h = Math.floor(ms / 3600000)
@@ -37,3 +34,11 @@ let m = Math.floor(ms / 60000) % 60
 let s = Math.floor(ms / 1000) % 60
 console.log({ms,h,m,s})
 return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
+
+handler.help = ['status']
+handler.tags = ['info']
+handler.command = /^(estado|status|estate|state|stado|stats)$/i
+//handler.register = true
+export default handler
+
+
