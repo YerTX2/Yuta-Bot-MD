38f65c41473c19db0bab83eccae2459625ec88d3
Diomar-s <diomarlugo405@gmail.com>
2024-11-08 00:42:35 -0400

    Update ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -18,7 +18,7 @@ return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, rcanal)
 handler.command = ['bard']
 handler.help = ['bard']
 handler.group = true;
-handler.register = true
+//handler.register = true
 handler.tags = ['ai']
 handler.premium = false
 export default handler
